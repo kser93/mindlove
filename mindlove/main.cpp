0a2f0d7aaedecb1c13263d19a91a1d45e941abd2
kser <kserx05@gmail.com>
2018-01-21 16:51:28 +0300

    BF code unprettified

--- mindlove/main.cpp
+++ mindlove/main.cpp
@@ -3,19 +3,17 @@
 #include <stack>
 #include <iterator>
 #include <string>
-#include <map>
 
 const std::vector<std::string> tests{ 
-	"++++++++[>+>++++<<-]>++>>+<[-[>>+<<-]+>>]>+[-<<<[->[+[-]+>++>>>-<<]<[<]>>++++++[<<+++++>>-]+<<++.[-]<<]>.>+[>>]>+]",
 	// "Hello world!\n"
-	"+++++ +++++             (initialize counter (cell #0) to 10)[                       (use loop to set the next four cells to 70/100/30/10)    > +++++ ++          (    add  7 to cell #1)    > +++++ +++++       (    add 10 to cell #2 )    > +++               (    add  3 to cell #3)    > +                 (    add  1 to cell #4)    <<<< -              (    decrement counter (cell #0))]                   > ++ .                  (print 'H')> + .                   (print 'e')+++++ ++ .              (print 'l').                       (print 'l')+++ .                   (print 'o')> ++ .                  (print ' ')<< +++++ +++++ +++++ .  (print 'W')> .                     (print 'o')+++ .                   (print 'r')----- - .               (print 'l')----- --- .             (print 'd')> + .                   (print bang)> .                     (print '\n')",
+	"++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.",
 };
 
 int eval(const std::string &);
 
 int main()
 {
-	return eval(tests[1]);
+	return eval(tests[0]);
 }
 
 int eval(const std::string &src)
